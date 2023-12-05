218552f25ead89b67f6c69ad832bff75e7893338
Dave Rayment <dave.rayment@gmail.com>
2023-12-05 02:33:23 +0000

    Part 2 of Day 3, plus refactored first part.

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,46 +1,60 @@
-﻿using System.Text.RegularExpressions;
-
-int total = 0;
+﻿// Note: a number may only have 1 neighbouring symbol
 var lines = File.ReadAllLines("Input.txt");
 int lineLen = lines[0].Length;
-char[,] schematic = new char[lines.Length, lineLen];
+
+List<PartNumber> numbers = [];
+
 for (int y = 0; y < lines.Length; y++)
 {
 	for (int x = 0; x < lineLen; x++)
 	{
-		schematic[y, x]	= lines[y][x];
-	}
-}
-
-for (int y = 0; y < lines.Length; y++)
-{
-	foreach (Match m in Regex.Matches(lines[y], @"\d+"))
-	{
-		for (int x = m.Index; x < m.Index + m.Length; x++)
+		char ch = lines[y][x];
+		if (char.IsDigit(ch))
 		{
-			if (IsBordered(schematic, y, x))
+			string numString = "";
+			PartNumber num = new();
+			do
 			{
-				total += int.Parse(m.Value);
-				break;
-			}
+				numString += lines[y][x];
+				num.SymbolLocation ??= GetSymbolLocation(y, x);
+				x++;
+			} while (x < lineLen && char.IsDigit(lines[y][x]));
+			num.Value = int.Parse(numString);
+			numbers.Add(num);
 		}
 	}
 }
 
-Console.WriteLine(total);
+Console.WriteLine(numbers.Where(x => x.SymbolLocation != null).Sum(x => x.Value));    // 546312
 
-bool IsBordered(char[,] schematic, int y, int x)
-{
-	return IsSymbol(schematic, y, x - 1) || IsSymbol(schematic, y, x + 1) ||
-		IsSymbol(schematic, y - 1, x - 1) || IsSymbol(schematic, y - 1, x) || IsSymbol(schematic, y - 1, x + 1) || IsSymbol(schematic, y, x + 1) ||
-		IsSymbol(schematic, y + 1, x - 1) || IsSymbol(schematic, y + 1, x) || IsSymbol(schematic, y + 1, x + 1);
-}
+// Gear-neighboring numbers.
+var wanted = numbers.Where(loc => {
+	if (loc.SymbolLocation == null) { return false; }
+	return lines[loc.SymbolLocation.Value.Y][loc.SymbolLocation.Value.X] == '*';
+});
 
-bool IsSymbol(char[,] schematic, int y, int x)
+// Group and sum the products of each group's values.
+long partTwoTotal = wanted.GroupBy(x => x.SymbolLocation)
+	.Where(x => x.Count() == 2)
+	.Sum(group => group.ElementAt(0).Value * group.ElementAt(1).Value);
+	//.Sum(x => x.Aggregate(1, (tot, num) => tot * num.Value));
+
+Console.WriteLine(partTwoTotal);    // 87449461
+
+// Get neighbouring symbol location, or null if not found.
+(int Y, int X)? GetSymbolLocation(int y, int x)
 {
-	if (y < 0 || y >= schematic.GetLength(0) || x < 0 || x >= schematic.GetLength(1))
+	foreach ((int dy, int dx) in new[] {
+		(0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1), (1, -1), (1, 0), (1, 1) })
 	{
-		return false;
+		int y1 = y + dy; int x1 = x + dx;
+		if (y1 >= 0 && y1 < lines.Length && x1 >= 0 && x1 < lineLen &&
+			lines[y1][x1] != '.' && !char.IsDigit(lines[y1][x1]))
+		{
+			return (y1, x1);
+		}
 	}
-	return schematic[y, x] != '.' && !char.IsDigit(schematic[y, x]);
+	return null;
 }
+
+record PartNumber { public int Value; public (int Y, int X)? SymbolLocation; }
