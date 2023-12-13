c8cd0ef777b9fdb968546ea00265e70688673d8b
Dave Rayment <dave.rayment@gmail.com>
2023-12-13 00:14:24 +0000

    Day 9 part one

---
+++ Day 9/Program.cs
@@ -0,0 +1,23 @@
+﻿long total = 0;
+
+foreach (string line in File.ReadAllLines("Input.txt"))
+{
+	List<IEnumerable<int>> triangle = [];
+	var working = line.Split(' ').Select(int.Parse);
+
+	do
+	{
+		triangle.Add(working);
+		working = working.Zip(working.Skip(1), (a, b) => b - a).ToList();
+	} while (!working.All(x => x == 0));
+
+	int toAdd = 0;
+	for (int i = triangle.Count - 1; i >= 0; i--)
+	{
+		toAdd += triangle[i].Last();
+	}
+
+	total += toAdd;
+}
+
+Console.WriteLine(total);   // 1974232246
